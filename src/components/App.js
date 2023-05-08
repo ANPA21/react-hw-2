bbb3b62389b525fed7eae2b725fae2bfa42d9629
Andrew <papsujevicandrew@gmail.com>
2023-05-08 18:40:12 +0300

    feedback only

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import Contacts from './Contacts/Contacts';
 import Feedback from './Feedback/Feedback';
 
 export const App = () => {
   return (
     <div>
       <Feedback />
-      <Contacts />
     </div>
   );
 };
--- src/components/Contacts/ContactList/ContactList.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-export const List = styled.ul`
-  background-color: ${p => p.theme.colors.accent};
-  list-style: none;
-  padding: 0;
-`;
